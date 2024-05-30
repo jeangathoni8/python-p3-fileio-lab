76e6db4c1f6ae636de7579c9a3b4ebd6dc2853ae
jeangathoni8 <jeangathoni39@gmail.com>
2024-05-30 11:10:17 +0300

    "Implemented write_file, append_file, and read_file functions in file_io.py"

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_name, content):
+    """Write content to a file."""
+    with open(f'{file_name}.txt', 'w') as f:
+        f.write(content)
 
-def append_file(file_name, append_content):
-    pass
+def append_file(file_name, content):
+    """Append content to a file."""
+    with open(f'{file_name}.txt', 'a') as f:
+        f.write(content)
 
 def read_file(file_name):
-    pass
+    """Read content from a file."""
+    with open(f'{file_name}.txt', 'r') as f:
+        return f.read()
+
